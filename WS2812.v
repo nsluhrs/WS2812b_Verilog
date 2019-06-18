97af851222d2e55ed71a8217bf1e3f69d09ee2f7
Nikolaus Luhrs <nikolaus.luhrs@gmail.com>
2019-06-18 17:04:54 -0600

    Updated to not devide clock and change ram addressing for better stability

--- WS2812.v
+++ WS2812.v
@@ -9,25 +9,39 @@ module WS2812(iSIGNAL_CLOCK,iDATA_CLOCK,dR, dG, dB,oCTRL_OUT, LED_IDX);
 	input [8:0] LED_IDX;
 	
 	output 		oCTRL_OUT;
-	
-	parameter T0H = 8;
-	parameter T1H = 16; 
-	parameter T0L = 17;
-	parameter T1L = 9;
-	parameter TRS = 110;
-	
+	//the following is times specified by the ws2812b datasheet
+	parameter T0H = 40;
+	parameter T1H = 80; 
+	parameter T0L = 85;
+	parameter T1L = 45;
+	parameter TRS = 5100;
+	parameter TIMER_BITS = 5100;
 
 	
 
 //signal declarations
 wire [23:0] wRamData;
-assign wRamData={dG,dR,dB};
+
 wire [23:0] wCDATA;
 wire true;
-assign true=1;
+
+//register declarations
 reg wRCLK;
 reg [8:0] read_address;
-reg clk20;
+reg [8:0] ledCount;
+reg [12:0] signal_timer;
+reg [4:0] bit_counter;
+reg [23:0] srCDATA;
+reg rCTRL_OUT;
+reg sig_reset;
+reg [6:0] TH;
+reg [6:0] TE;
+//assignments
+assign true=1;
+assign oCTRL_OUT=rCTRL_OUT;
+assign wRamData={dG,dR,dB};
+
+
 RAM2P ram(
 	.data(wRamData),
 	.wraddress(LED_IDX),
@@ -35,12 +49,17 @@ RAM2P ram(
 	.wren(true),
 	
 	.rdaddress(read_address),
-	.rdclock(clk20),
+	.rdclock(iSIGNAL_CLOCK),
 	.q(wCDATA)
 );
 
-reg [8:0] ledCount;
 
+/*
+not 100% sure how to do this but the idea is that the higher the number
+of LEDs indexed the larger the number of LEDs the program will loop through
+this should give the fastest possible write rate Should probably impliment
+a reset
+*/
 always @(LED_IDX)
 begin
 	if (LED_IDX+1 > ledCount && LED_IDX)
@@ -49,32 +68,12 @@ begin
 	end
 end
 
-reg [6:0] signal_timer;
-
-
-reg [4:0] bit_counter;
-reg [23:0] srCDATA;
-reg rCTRL_OUT;
-assign oCTRL_OUT=rCTRL_OUT;
-reg sig_reset;
-reg [2:0] clk_div;
 
 initial 
 begin
 	clk_div=0;
 end
-always @(posedge iSIGNAL_CLOCK)
-begin
-	clk_div=clk_div+1;
-	if (clk_div==5)
-	begin
-		clk_div=0;
-	end
-	clk20=(clk_div<3)?1:0;
 
-end	
-reg [6:0] TH;
-reg [6:0] TE;
 
 initial 
 begin
@@ -83,56 +82,61 @@ ledCount=0;
 bit_counter=0;
 signal_timer=0;
 end
-always @(posedge clk20)
+always @(posedge iSIGNAL_CLOCK)
 begin
 	if (signal_timer==0 && ~sig_reset)
 	begin
 		rCTRL_OUT<=1;
 	end
-	if (srCDATA[23])
-	begin
-		TH=T1H;
-		TE=T1H+T1L;
-	end
-	else
-	begin
-		TH=T0H;
-		TE=T0H+T0L;
-	end
-	rCTRL_OUT=(signal_timer < TH)?1:0;
-	if (signal_timer >= TE)
-	begin
-		srCDATA<=srCDATA<<1;
-		signal_timer<=0;
-		if (bit_counter == 23)
+	if (~sig_reset) begin
+		if (srCDATA[23])
 		begin
-			bit_counter <= 0;
-			if (read_address >= ledCount)
-			begin
-				read_address<=0;
-			end
-			else
-			begin
-				read_address<=read_address+1;
-			end
+			TH=T1H;
+			TE=T1H+T1L;
 		end
 		else
 		begin
-			bit_counter<=bit_counter+1;
+			TH=T0H;
+			TE=T0H+T0L;
+		end
+		rCTRL_OUT=(signal_timer < TH)?1:0;
+		if (signal_timer >= TE)
+		begin
+			srCDATA<=srCDATA<<1;
+			signal_timer<=0;
+			if (bit_counter == 11) begin//change read address here
+				if (read_address >= ledCount) begin
+					read_address<=0;
+				end
+				else begin
+					read_address<=read_address+1;
+				end
+			end
+			if (bit_counter == 23)//read from ram output here
+			begin
+				bit_counter <= 0;
+				srCDATA <= wCDATA;
+				if (read_address == 0) begin
+					sig_reset <= 1;
+				end
+			end
+			else
+			begin
+				bit_counter<=bit_counter+1;
+			end
 		end
 	end
-		
+	if (sig_reset && signal_timer>TRS) begin
+		signal_timer <= 0;
+		sig_reset <= 0;
+		srCDATA <= wCDATA;
+	end
 	
 	signal_timer=signal_timer+1;
 end
 
 
-always @(read_address)
-begin
-	wRCLK=1;
-	#5 srCDATA = wCDATA;
-	#10 wRCLK=0;
-end
+
 
 endmodule
 	
